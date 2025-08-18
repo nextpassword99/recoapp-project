677be417f26906c82060d4953ca7acdbf2e5844c
nextpassword99 <inderscop@gmail.com>
2025-08-18 15:52:23 -0500

    feat: Agregar un conversor para date

---
+++ app/src/main/java/com/example/recoapp/data/Converters.kt
@@ -0,0 +1,11 @@
+package com.example.recoapp.data
+
+import androidx.room.TypeConverter
+import java.util.Date
+
+class Converters {
+    @TypeConverter
+    fun fromTimestamp(value: Long?): Date? {
+        return value?.let { Date(it) }
+    }
+}
