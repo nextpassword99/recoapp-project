31f5a5b4463e6e8fecdeec77dd4a2d3f72032789
nextpassword99 <inderscop@gmail.com>
2025-08-18 12:33:12 -0500

    feat: Agregar el DAO de la entidad Waste

---
+++ app/src/main/java/com/example/recoapp/data/WasteDao.kt
@@ -0,0 +1,19 @@
+package com.example.recoapp.data
+
+import androidx.room.*
+
+interface WasteDao {
+    @Insert
+    suspend fun insert(waste: Waste)
+
+    @Update
+    suspend fun update(waste: Waste)
+
+    @Delete
+    suspend fun delete(waste: Waste)
+
+    @Query("SELECT * FROM waste ORDER BY date DESC")
+    suspend fun query(waste: Waste)
+
+    suspend fun getAllWaste(): List<Waste>
+}
