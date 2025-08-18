506ac4bcd2e558f8c336dc4191803f5c35434f35
nextpassword99 <inderscop@gmail.com>
2025-08-18 17:16:02 -0500

    feat: Agregar método para convertir inverso de la fecha

--- app/src/main/java/com/example/recoapp/data/Converters.kt
+++ app/src/main/java/com/example/recoapp/data/Converters.kt
@@ -8,4 +8,9 @@ class Converters {
     fun fromTimestamp(value: Long?): Date? {
         return value?.let { Date(it) }
     }
+
+    @TypeConverter
+    fun dateToTimestamp(date: Date?): Long? {
+        return date?.time
+    }
 }
