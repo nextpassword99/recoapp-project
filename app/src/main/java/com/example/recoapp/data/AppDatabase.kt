9b8bb34dcac4b22d11a5f753b05600e84f918593
nextpassword99 <inderscop@gmail.com>
2025-08-18 15:51:42 -0500

    feat: Agregar la base de datos

---
+++ app/src/main/java/com/example/recoapp/data/AppDatabase.kt
@@ -0,0 +1,33 @@
+package com.example.recoapp.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+
+@Database([Waste::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun wasteDao(): WasteDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "recoapp_database"
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+
+
+}
