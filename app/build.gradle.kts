e450c85e966c0fd9bd8df3c0cb8ca3b139d85526
nextpassword99 <inderscop@gmail.com>
2025-08-18 19:43:10 -0500

    feat: Implementar dependencias de material UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,5 +66,7 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.compose.material3:material3:1.1.2")
+    implementation("com.google.android.material:material:1.10.0")
 
 }
