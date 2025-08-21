88583bd7324efe0fc2defbfd9f13e4aba1002980
nextpassword99 <inderscop@gmail.com>
2025-08-21 16:25:36 -0500

    feat: Agregar incializador de la función de inicio para el activity de autenticación

--- app/src/main/java/com/example/recoapp/MainActivity.kt
+++ app/src/main/java/com/example/recoapp/MainActivity.kt
@@ -5,11 +5,15 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.example.recoapp.auth.SessionManager
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var session: SessionManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        session = SessionManager(this)
 
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
@@ -29,6 +33,14 @@ class MainActivity : AppCompatActivity() {
 
         btnToReports.setOnClickListener {
             startActivity(Intent(this, ReportsActivity::class.java))
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (this::session.isInitialized && session.fetchAuthToken() == null) {
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
         }
     }
 }
